11ec98d104b5e8dcc5b49ebf4d4023c39ed409c4
finote2021 <finotepawloss29@gmail.com>
2024-07-16 20:17:32 -0700

    Carousel added

--- amazonclone-2024/src/App.jsx
+++ amazonclone-2024/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import LowerHeader from './Components/Header/LowerHeader';
+import  Carousel  from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <div className="">
       <Header />
+      <LowerHeader/>
+      <Carousel />
+
     </div>
   );
 }
---
+++ amazonclone-2024/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./img/data.js";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader we  need to installl npm i react-responsive-carousel firts 
+function CarouselEffect() {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteloop={true}
+        showindicator={true}
+        showThumbs={true}
+      >
+        {img.map((imageItemslink) => {
+          return <img src={imageItemslink} />;
+        })}
+      </Carousel>
+      <div className="hero_img"> </div>
+    </div>
+  );
+}
+
+export default CarouselEffect;
